a197e4339c6fed5337f6f22d157cd38f41c5837c
unknownHPW <william.perin@escola.pr.gov.br>
2025-09-04 10:24:27 -0300

    Delete tabuada.js

--- tabuada.js
+++
@@ -1,8 +0,0 @@
-function gerarTabuada() {
-  const numero = parseInt(document.getElementById("numero").value);
-  const resultado = document.getElementById("resultado");
-  resultado.innerHTML = `<h2>Tabuada do ${numero}</h2>`; 
-  for (let i = 1; i <= 10; i++) {
-    resultado.innerHTML += `${numero} x ${i} = ${numero * i}<br>`;
-  }
-}
